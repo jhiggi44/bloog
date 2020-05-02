5ee3a7ee0b5d2377469e004c90dd7920f6194b17
jhiggi44 <jordandhiggins@gmail.com>
2020-05-01 18:32:00 -0600

    persist post to active record through form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,13 @@
 
 class PostsController < ApplicationController
   def create
-   redirect_to "/creators/#{params[:creator_id]}" if Post.new(title: params[:title], content: params[:content])
+    creator_id = params[:creator_id]
+    post = params[:post]
+    redirect_to "/creators/#{creator_id}" if Post.new(creator_id: creator_id, title: post[:title], content: post[:content]).save
   end
 
-  def new;end
+  def new
+    @creator = Creator.find(params[:creator_id])
+    @post = Post.new(creator_id: @creator.id)
+  end
 end
--- test/controllers/creators_test.rb
+++ test/controllers/creators_test.rb
@@ -16,14 +16,15 @@ class CreatorsTest < ActionDispatch::IntegrationTest
 
     assert_equal 200, status
 
-    assert_select "form[action=?]", "/creators/#{creator[:id]}/posts/new"
+    assert_select "form[action=?]", "/creators/#{creator[:id]}/posts"
+    assert_select "form[method=?]", "post"
 
     assert_select "label", "Title" do
-      assert_select 'input#title'
+      assert_select 'input#post_title'
     end
 
     assert_select "label", "Content" do
-      assert_select 'textarea#content'
+      assert_select 'textarea#post_content'
     end
 
     assert_select "input[value=?]", "Create Post"
@@ -31,7 +32,7 @@ class CreatorsTest < ActionDispatch::IntegrationTest
 
   test "create" do
     creator = creator(:thor)
-    post "/creators/#{creator[:id]}/posts", params: { title: "A Title", content: "Some good content." }
+    post "/creators/#{creator[:id]}/posts", params: { post: { title: "A Title", content: "Some good content." } }
     follow_redirect!
     assert_equal 200, status
     assert_equal "/creators/#{creator[:id]}", path
