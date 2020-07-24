dc73dd758ce60e5d9c8a6662f6e714280b8fd0f8
jhiggi44 <jordandhiggins@gmail.com>
2020-07-24 17:04:00 -0600

    Render sections based on content display value

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,7 @@
 class Section < ActiveRecord::Base
  belongs_to :post
+
+ def display
+  contents["display"]
+ end
 end 
--- test/controllers/posts_test.rb
+++ test/controllers/posts_test.rb
@@ -10,5 +10,18 @@ class PostsTest < ActionDispatch::IntegrationTest
   end
 
   test 'show' do
+    user = users(:thor)
+    post = posts(:thor_hero)
+    quote_section = post.sections[2]
+
+    get "/users/#{user.id}/posts/#{post.id}"
+    assert_select 'h2', post.title
+    assert_select 'h4', "By: #{user.name}"
+    assert_select 'h3', post.sections.first.contents["text"]
+    assert_select 'p', post.sections.second.contents["text"]
+
+    assert_select "p", quote_section.contents["quote"]
+    assert_select "blockquote[cite=?]", quote_section.contents["source"]
+    assert_select 'a[href=?]', quote_section.contents["source"], { text: quote_section.contents["author"] }
   end
 end
