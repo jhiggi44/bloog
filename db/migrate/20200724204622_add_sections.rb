8b4efcf830b581d3e5c2bc233484a83b0d464bb5
jhiggi44 <jordandhiggins@gmail.com>
2020-07-24 14:53:20 -0600

    Remove type in sections table

--- db/migrate/20200724204622_add_sections.rb
+++ db/migrate/20200724204622_add_sections.rb
@@ -1,7 +1,6 @@
 class AddSections < ActiveRecord::Migration[5.2]
   def change
     create_table :sections do |t|
-      t.string :type
       t.integer :position
       t.jsonb :contents, null: false, default: '{}'
       t.timestamps
