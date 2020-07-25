00d62c65cb75629fa8adce0478778b787e2c8ba0
jhiggi44 <jordandhiggins@gmail.com>
2020-07-24 18:08:30 -0600

    display posts by most recent

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :sections
+
+  def self.list_by_most_recent
+    all.sort_by(&:created_at).reverse
+  end
+
+  def url 
+    "/users/#{user.id}/posts/#{id}"
+  end
 end
