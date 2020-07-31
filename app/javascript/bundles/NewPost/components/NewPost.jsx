f4ea7081b37f7dac52c9c6701d6d2f9540bd8925
jhiggi44 <jordandhiggins@gmail.com>
2020-07-31 16:57:56 -0600

    Add temporary handle submit

--- app/javascript/bundles/NewPost/components/NewPost.jsx
+++ app/javascript/bundles/NewPost/components/NewPost.jsx
@@ -14,6 +14,29 @@ function reducer(state, action) {
   }
 }
 
+function handleSubmit(event, post, user_id) {
+ event.preventDefault();
+
+ const token = 
+      document.querySelector('[name=csrf-token]').content
+
+ fetch(`http://localhost:8080/users/${user_id}/posts`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-CSRF-TOKEN': token
+  },
+  body: JSON.stringify({ user_id: user_id, post: post }),
+ })
+ .then(response => response.json())
+ .then(data => {
+   console.log('Success:', data);
+ })
+ .catch((error) => {
+   console.error('Error:', error);
+ });
+}
+
 export default function NewPost() {
  const [state, dispatch] = useReducer(reducer, initialState);
 
@@ -33,7 +56,10 @@ export default function NewPost() {
     <input type="text" id="Title" defaultValue="Some Awesome Title" /><br/>
     <TextSectionInput position={0} updateSection={updateSection} />
     <TextSectionInput position={1} updateSection={updateSection} />
-    <input type="submit" defaultValue="Submit" onClick={(e) => { e.preventDefault(); console.log(state); }} />
+    <input 
+     type="submit" 
+     defaultValue="Submit" 
+     onClick={(e) => { handleSubmit(e, state, 1) }} />
    </form>
   </main>
  );
