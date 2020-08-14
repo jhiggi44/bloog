2f16722ceefa57849b1b21b76ac7835a9930a0f7
jhiggi44 <jordandhiggins@gmail.com>
2020-08-14 16:21:19 -0600

    Create posts with multiple sections

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,11 @@
 class PostsController < ApplicationController
   def create
     user_id = params[:user_id]
-    post = params[:post]
-    if Post.new(user_id: user_id, title: post[:title]).save
-      redirect_to "/users/#{user_id}"
-    end
+    post_details = params[:post]
+    post = Post.create(user_id: user_id, title: post_details[:title])
+    create_sections_of_post(post, post_details[:sections])
+
+    redirect_to "/users/#{user_id}"
   end
 
   def new
@@ -17,5 +18,11 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @sections = @post.sections
+  end
+
+  def create_sections_of_post(post, sections)
+    sections.each_with_index do |section, index|
+      Section.create(post: post, contents: section, position: index)
+    end
   end
 end
